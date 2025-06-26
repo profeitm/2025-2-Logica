2e3eff36811aca17c910d9cfd07aceeea0406ffa
profeitm <profeitm@gmail.com>
2025-06-26 11:09:09 -0500

    Subiendo la Practica 04 con archivos

---
+++ Practica_04/lpe2_20250626_s04_01.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+lpe2_20250626_s04_01.c
+Descripción: Programa para Saludar
+
+...
+Autor:
+Fecha:
+Versión:
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hola a todos los que están en el Curso, me llamo Alexander");
+
+    return 0;
+}
